afe0bd5eaf621d8df570db504dbad6c1f2fac6c4
Miheret Girmachew <124549518+Miheret-Girmachew@users.noreply.github.com>
2023-12-01 12:22:33 +0300

    Added solution for Number of Good Pairs

---
+++ leet-code-solutions/number-of-good-pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count=0
+        for i in range(len(nums)-1):
+            for j in range(len(nums)-1,0,-1):
+                if nums[i]==nums[j] and i<j:
+                    count+=1
+                    
+        return count
+            
