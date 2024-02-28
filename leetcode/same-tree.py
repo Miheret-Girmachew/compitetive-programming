edd8189f18e85bb6bc27ced000f7224a4f50af08
Miheret Girmachew <124549518+Miheret-Girmachew@users.noreply.github.com>
2024-02-28 18:37:04 +0300

    Add solution for Same Tree

---
+++ leetcode/same-tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def traverse(p, q):
+            if not p and not q:
+                return True
+                
+            elif not p or not q:
+                return False
+        
+                
+            elif p.val != q.val:
+                
+                return False
+                
+            else:
+                return p.val == q.val and traverse(p.left, q.left) and traverse(p.right, q.right)
+        
+        return traverse(p,q)
+             
+       
+
+        
